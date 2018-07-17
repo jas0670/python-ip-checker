ec13786513bc643e1cb6234f0f31447648e42495
Jonathan Roosa <jonathan.roosa@wipro.com>
2018-07-17 16:04:00 -0500

    - Add excel row starting for IP analysis - Start at beginning or at last checked if new IPs in list - Add filename input - Spaces to tabs - Removed RoboBrowser

--- ipc.py
+++ ipc.py
@@ -2,13 +2,30 @@ import xlrd
 import re
 import warnings
 warnings.filterwarnings("ignore",category=UserWarning, module='bs4')
-from robobrowser import RoboBrowser
 
-#pull info from excel and prints out relevant ip info to screen.
-workbook = xlrd.open_workbook('ReferenceData_Test_ThreatConnect Botnet IPs Results_asc.xlsx') #,on_demand = True)
-sheet = workbook.sheet_by_index(0) #first sheet of excel
-for x in range (0,sheet.ncols):
- for y in range (1,sheet.nrows):
-  if sheet.cell_value(y,x) != "":
-   print(sheet.cell_value(y,x))
-   
+# Pull info from excel and print out relevant IP info to screen
+
+# Start by getting Excel filename and opening it, get first sheet
+filename = input("Enter excel filename with extension (xls, xlsx): ")
+workbook = xlrd.open_workbook(filename) #,on_demand = True)
+sheet = workbook.sheet_by_index(0) 
+
+# Menu options
+choice = 0
+while choice < 1 or choice > 3:
+	print("\n*** Wipro Python IP Checker ***\n")
+	print("		1) Start from beginning")
+	print("		2) Start from last checked")
+	print("		3) Exit\n")
+	choice = int(input("  > "))
+
+# Set starting row based off user choice
+startrow = 1
+if choice == 2:
+	for x in range(1, sheet.nrows):
+		if (sheet.cell_type(x, 1) == xlrd.XL_CELL_EMPTY):
+			startrow = x + 1
+			break
+elif choice == 3:
+	exit()
+
