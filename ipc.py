45aa03f6f1dde6b8f49582ad0f8b3306a7607a8a
Jonathan Roosa <jonathan.roosa@wipro.com>
2018-07-18 16:07:04 -0500

    Add Xforce Exchange API request functionality for saving scores to spreadsheet

--- ipc.py
+++ ipc.py
@@ -1,31 +1,89 @@
-import xlrd
+#import openpyxl
 import re
-import warnings
-warnings.filterwarnings("ignore",category=UserWarning, module='bs4')
+#import warnings
+import grequests
+import requests
+import time
+import os
+from openpyxl import *
 
-# Pull info from excel and print out relevant IP info to screen
+class IPC():
+	def __init__(self):
+		self.startrow = 2
+		self.filename = ''
+		self.workbook = ''
+		self.sheet = ''
 
-# Start by getting Excel filename and opening it, get first sheet
-filename = input("Enter excel filename with extension (xls, xlsx): ")
-workbook = xlrd.open_workbook(filename) #,on_demand = True)
-sheet = workbook.sheet_by_index(0) 
+	def load(self):
+		# Start by getting Excel filename and opening it, get first sheet
+		self.filename = input("Enter excel filename with extension (xls, xlsx): ")
+		self.workbook = load_workbook(self.filename) #,on_demand = True)
+		self.sheet = self.workbook.active
 
-# Menu options
-choice = 0
-while choice < 1 or choice > 3:
-	print("\n*** Wipro Python IP Checker ***\n")
-	print("		1) Start from beginning")
-	print("		2) Start from last checked")
-	print("		3) Exit\n")
-	choice = int(input("  > "))
+		# Menu options
+		choice = 0
+		while choice < 1 or choice > 3:
+			print("\n*** Wipro Python IP Checker ***\n")
+			print("		1) Start from beginning")
+			print("		2) Start from last checked")
+			print("		3) Exit\n")
+			choice = int(input("  > "))
 
-# Set starting row based off user choice
-startrow = 1
-if choice == 2:
-	for x in range(1, sheet.nrows):
-		if (sheet.cell_type(x, 1) == xlrd.XL_CELL_EMPTY):
-			startrow = x + 1
-			break
-elif choice == 3:
-	exit()
+		# Set starting row based off user choice
+		self.startrow = 2
+		if choice == 2:
+			for x in range(2, self.sheet.max_row + 1):
+				print(self.sheet.cell(x, 2))
+				if self.sheet.cell(x, 2).value is None:
+					self.startrow = x
+					break
+		elif choice == 3:
+			exit()
 
+	# Pulls data from the Xforce Exchange API
+	def xforce(self):
+		# Collect list of all Xforce Exchange API URLs for each IP
+		# Due to restrictions of Xforce Exchange API, only request 10 IPs at a time
+		# Code will gather all urls in one list, and then split into chunks for sending 10 at a tim
+		responses = []
+		urls = []
+		for row in self.sheet.iter_rows(min_row=self.startrow, max_col=1, max_row=self.sheet.max_row):
+			for cell in row:
+				urls.append("https://api.xforce.ibmcloud.com/ipr/history/{}".format(cell.value))
+
+		url_lists = [urls[x:x+10] for x in range(0, len(urls), 10)]
+		for l in url_lists:
+			request_list = (grequests.get(u, timeout=1000) for u in l)
+			print("Sending request of {} IPs...".format(len(l)))
+			responses.extend(grequests.map(request_list))
+
+		scores = []
+		for r in responses:
+			try:
+				scores.append(r.json()['history'][-1]['score'])
+			except:
+				scores.append("Timeout")
+
+		# Add scores back to spreadsheet and save
+		for cell in [x for t in self.sheet['B{}'.format(self.startrow):'B{}'.format(self.sheet.max_row)] for x in t]:
+			cell.value = scores.pop(0)
+
+	# Pulls data from the URLVoid API
+	def urlvoid(self):
+		# Put Code here
+		return None
+
+	# Pulls data from the TotalVirus API
+	def totalvirus(self):
+		# Put Code here
+		return None
+	
+	# Save the workbook
+	def save(self):
+		self.workbook.save(self.filename)
+
+
+checker = IPC()
+checker.load()
+checker.xforce()
+checker.save()
