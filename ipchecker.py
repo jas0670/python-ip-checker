1e196a6222566e995e4ac3b1a274546e155a2bcb
jas0670 <james.sabetti@wipro.com>
2018-07-26 14:40:51 -0400

    Updated, now utilizes secondary sheet

--- ipchecker.py
+++ ipchecker.py
@@ -15,17 +15,32 @@ ip = 'i'
 
 print_value = str(value)
 cell = letter+print_value
+new_cell = letter+print_value
+second_value = value #this is for the second sheet
 
 while ws[cell].value: #basically, while the cell has an obtainable value, do this
-	print_value = str(value)
-	cell = letter+print_value
-	ip = str(ws[cell].value)
-	print(ip)
+	print_value = str(value) #needed so the workbook can grab the appropriate cell 
+	cell = letter+print_value #the cell in question
+	ip = str(ws[cell].value) #the ip in question
+	print(ip) #so we can keep track of what IP's are going through the system,. Mainly for testing purposes
 	if ws[cell].value is None: #if there is no obtainable information
 		break
-	ip_report = os.system('./blcheck ' + ip)
-	child = subprocess.Popen(ip_report,shell=True,stdout=subprocess.PIPE)
+	ip_report = ('./blcheck ' + ip) #holds the command block to be executed
+	child = subprocess.Popen(ip_report,shell=True,stdout=subprocess.PIPE) #execution of said commmand and the gathering of its output
 	output = str(child.communicate()[0])
 	output = re.findall(r'\d+',output)
-	print(int(output[0]))
-	value += 1
+	response = (int(output[0]))
+	if response > 0:
+		#this following block takes the information and puts it into the second sheet of the excel document
+		new_cell = letter+str(second_value) #reassignment back to the first column
+		ss[new_cell].value = ip #stores the ip in the second sheet
+		cell = 'B'+print_value # makes a new cell for the home sheet
+		ws[cell].value = 'No' #stores that the ip address is not safe
+		new_cell = 'B'+str(second_value) #reassignment to the next column
+		ss[new_cell].value = response #puts the number of blacklists in the second sheet
+		second_value += 1
+	else:
+		cell = 'B'+print_value
+		ws[cell].value = 'Yes'
+	value += 1 #goes to the next spot in the excel sheet
+	wb.save('example_testfile.xlsx')
