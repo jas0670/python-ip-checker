69df8c2bc476592c6b840dc5db3d816299f42071
root <root@localhost.localdomain>
2018-07-26 11:05:42 -0500

    modified shell script to only give us what we need

--- runShell.py
+++ runShell.py
@@ -1,3 +1,14 @@
 import os, sys
 ip = input("Enter IP:\n")
-os.system("./blcheck " + ip)
+#os.system("./blcheck " + ip)
+command = "./blcheck " + ip
+
+import subprocess
+import re
+
+child = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE)
+output = str(child.communicate()[0])
+
+
+output = re.findall(r'\d+', output)
+print(int(output[0]))
