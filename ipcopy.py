87d5d8337f040981090bafe39ddb006b245513cb
jas0670 <james.sabetti@wipro.com>
2018-07-27 17:26:55 -0400

    Update ipcopy.py

--- ipcopy.py
+++ ipcopy.py
@@ -3,7 +3,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 wb = openpyxl.load_workbook('example_testfile.xlsx')
-ws = wb['Sheet2']
+ws = wb['Sheet1']
 
 value = 2
 letter = 'A'
@@ -23,7 +23,7 @@ while ws[cell].value:
 	response = requests.get("https://api.xforce.ibmcloud.com/ipr/history/"+ip, auth=HTTPBasicAuth('f0d4525f-a4da-42ef-ba45-17d82d812531', '92209c34-896d-4f08-b787-723f2b71b336'))
 	print(response)
 	response_data = response.json()
-	cell = 'C'+print_value
+	cell = 'D'+print_value
 	ws[cell].value = ''.join(list(response_data['history'][-1]['categoryDescriptions'].keys()))
 	value += 1
 	print("end of loop")
