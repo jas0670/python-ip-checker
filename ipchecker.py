9d288d1a8141b33c30c863d13398c9dd19f1328f
jas0670 <james.sabetti@wipro.com>
2018-07-27 17:26:11 -0400

    Update ipchecker.py

--- ipchecker.py
+++ ipchecker.py
@@ -4,14 +4,6 @@ Need to have installed from pip:
 - openpyxl
 '''
 
-
-'''
-This requires python3, make sure you have pip3 installed as well
-Need to have installed from pip:
-- openpyxl
-'''
-
-
 import os,sys #needed to run  bash scripts
 import openpyxl #needed to mess with excel
 import time #needed to make a pause
@@ -32,7 +24,9 @@ cell = letter+print_value
 new_cell = letter+print_value
 second_value = value #this is for the second sheet
 
-while ws[cell].value: #basically, while the cell has an obtainable value, do this
+
+while value<15:
+#while ws[cell].value: #basically, while the cell has an obtainable value, do this
 	print_value = str(value) #needed so the workbook can grab the appropriate cell 
 	cell = letter+print_value #the cell in question
 	ip = str(ws[cell].value) #the ip in question
@@ -43,16 +37,13 @@ while ws[cell].value: #basically, while the cell has an obtainable value, do thi
 	child = subprocess.Popen(ip_report,shell=True,stdout=subprocess.PIPE) #execution of said commmand and the gathering of its output
 	output = str(child.communicate()[0])
 	output = re.findall(r'\d+',output)
-	response = (int(output[0]))
+	response = (int(output[-1]))
 	if response > 0:
 		#this following block takes the information and puts it into the second sheet of the excel document
-		new_cell = letter+str(second_value) #reassignment back to the first column
-		ss[new_cell].value = ip #stores the ip in the second sheet
 		cell = 'B'+print_value # makes a new cell for the home sheet
 		ws[cell].value = 'No' #stores that the ip address is not safe
-		new_cell = 'B'+str(second_value) #reassignment to the next column
-		ss[new_cell].value = response #puts the number of blacklists in the second sheet
-		second_value += 1
+		cell = 'C'+print_value #moves to the next column
+		ws[cell].value = response #puts the number of blacklists in the home sheet
 	else:
 		cell = 'B'+print_value
 		ws[cell].value = 'Yes'
